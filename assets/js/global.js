838d52fd51c9364f6508d7b6c23a45d950aa46f8
Akshay Dahiya <thinkbots.dcrst@gmail.com>
2016-10-10 20:59:13 +0530

    Shift launch date

--- assets/js/global.js
+++ assets/js/global.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
   ** Accepted format: MM/DD/YYYY HH:MM  E.g: 10/25/2016 20:00
   ** Hours and minutes are optional
   */
-  clock.initializeClock('timediv',"10/15/2016");
+  clock.initializeClock('timediv',"10/28/2016");
 
   // Preload screen fade out when background image is downloaded
   var $div = $('.wrapper'), bg = $div.css('background-image');
